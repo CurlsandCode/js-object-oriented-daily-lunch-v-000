fcb2d24c47960bb8c6a16fa09dfe1763a77760c0
Monika Williams <monika.m.williams@gmail.com>
2017-12-12 12:56:55 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,50 @@
+let store = {customers:[], meals:[], deliveries:[], employers:[]};
+
+let customerId = 0;
+class Customer {
+  constructor(name, employer){
+    this.name = name;
+    this.id = ++customerId;
+    if (employer){
+      this.employerId = employer.id;
+    }
+    store.customers.push(this);
+  }
+}
+
+let mealId = 0;
+class Meal {
+  constructor(title, price){
+    this.title = title;
+    this.price = price;
+    this.id = ++mealId;
+    
+    store.meals.push(this);
+  }
+}
+
+
+let deliveryId = 0;
+class Delivery {
+  constructor(meal, customer){
+    this.id = ++deliveryId;
+    if (meal){
+      this.mealId = meal.id;
+    }
+    if (customer){
+      this.customerId = customer.id;
+    }
+    store.deliveries.push(this);
+  }
+}
+
+
+let employerId = 0;
+class Employer {
+  constructor(name){
+    this.name = name;
+    this.id = ++employerId;
+    
+    store.employers.push(this);
+  }
+}
